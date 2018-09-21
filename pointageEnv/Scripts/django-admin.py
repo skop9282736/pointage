4191827ef1992dfed394be22ba884ff442f6264e
ismail aidar <slimaidarismail@gmail.com>
2018-09-21 11:20:20 +0100

    remove env from main

--- pointageEnv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\slima\desktop\projects\pointage\pointageenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
